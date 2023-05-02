70fda8bd923c3fc9b3c2f64d8d40bfe2daca16c6
Celia Cabello Navarro <118032871+celiacnavarro@users.noreply.github.com>
2023-05-02 08:34:50 +0200

    Update task_12.sql

--- sql_queries/task_12.sql
+++ sql_queries/task_12.sql
@@ -1,5 +1,5 @@
-#Write an SQL query to solve the given problem statement.
-#Are we overrun with bots and celebrity accounts? Find the percentage of our users who have either never commented on a photo or have commented on every photo
+-- Write an SQL query to solve the given problem statement.
+-- Are we overrun with bots and celebrity accounts? Find the percentage of our users who have either never commented on a photo or have commented on every photo
 
 SELECT tableA.total_A AS 'Number Of Users who never commented',
 (tableA.total_A/(SELECT COUNT(*) FROM users))*100 AS '%',
@@ -7,4 +7,16 @@ tableB.total_B AS 'Number of Users who likes on every photos',
 (tableB.total_B/(SELECT COUNT(*) FROM users))*100 AS '%'FROM(SELECT COUNT(*) AS total_A 
 FROM(SELECT username,comment_text 
 FROM users 
-LEFT JOIN comments ON users.id =comments.user_id GROUP BY users.id HAVING comment_text IS NULL) AS total_number_of_users_without_comments) AS tableA JOIN(SELECT COUNT(*) AS total_B FROM(SELECT user_id as id,username,count(*) as total_likes_by_user FROM likes inner join users on likes.user_id=users.id GROUP BY user_id HAVING COUNT(*) = (SELECT COUNT(*) FROM photos)) AS total_number_users_with_likes)AS tableB;
+LEFT JOIN comments 
+ON users.id =comments.user_id 
+GROUP BY users.id 
+HAVING comment_text IS NULL) 
+AS total_number_of_users_without_comments)
+AS tableA JOIN(SELECT COUNT(*) AS total_B 
+               FROM(SELECT user_id as id,username,count(*) as total_likes_by_user 
+                    FROM likes inner 
+                    JOIN users 
+                    ON likes.user_id=users.id 
+                    GROUP BY user_id 
+                    HAVING COUNT(*) = (SELECT COUNT(*) 
+                                       FROM photos)) AS total_number_users_with_likes)AS tableB;
