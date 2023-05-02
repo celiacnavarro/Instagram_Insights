987ce8cb8fceade945b9e1f411be738794fae3f2
Celia Cabello Navarro <118032871+celiacnavarro@users.noreply.github.com>
2023-05-02 08:19:41 +0200

    Update task_1.sql

--- sql_queries/task_1.sql
+++ sql_queries/task_1.sql
@@ -2,14 +2,7 @@
 -- What day of the week do most users register on? 
 -- We need to figure out when to schedule an ad campgain
 
-SELECT 
-    DAYNAME(created_at) AS day_of_week, 
-    COUNT(*) AS count
-FROM 
-    users 
-GROUP BY 
-    day_of_week 
-ORDER BY 
-    count DESC 
-LIMIT 
-    1;
+SELECT id, username, created_at
+FROM users 
+ORDER BY created_at 
+ASC LIMIT 5
