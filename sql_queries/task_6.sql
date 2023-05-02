9516c76be23e9d9dec93d0ae23bc4579e44473b2
Celia Cabello Navarro <118032871+celiacnavarro@users.noreply.github.com>
2023-05-02 08:30:38 +0200

    Update task_6.sql

--- sql_queries/task_6.sql
+++ sql_queries/task_6.sql
@@ -1,9 +1,9 @@
--- Write an SQL query to solve the given problem statement.
--- Total Posts by users (longer versionof SELECT COUNT(*)FROM photos)
+--  Write an SQL query to solve the given problem statement.
+--  user ranking by postings higher to lower
 
-SELECT SUM(post_count) AS total_posts
-FROM (
-  SELECT COUNT(*) AS post_count
-  FROM photos
-  GROUP BY user_id
-) subquery;
+SELECT users.username, COUNT(photos.image_url)
+FROM users
+LEFT JOIN photos ON users.id = photos.user_id
+GROUP BY users.id
+ORDER BY COUNT(photos.image_url) DESC
+LIMIT 74;
