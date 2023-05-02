3c1bf452dfa91fa70bbfe7d27ae9c03aaf54c057
Celia Cabello Navarro <118032871+celiacnavarro@users.noreply.github.com>
2023-05-02 08:32:21 +0200

    Update task_10.sql

--- sql_queries/task_10.sql
+++ sql_queries/task_10.sql
@@ -1,8 +1,9 @@
 -- Write an SQL query to solve the given problem statement.
--- We also have a problem with celebrities. Find users who have never commented on a photo
+-- We have a small problem with bots on our site. Find users who have liked every single photo on the site
 
-SELECT username, comments.comment_text
+SELECT users.id, users.username, COUNT(DISTINCT photo_id) AS total_likes_by_user
 FROM users
-LEFT JOIN comments
-ON users.id = comments.user_id
-WHERE comments.comment_text IS NULL;
+JOIN likes
+ON users.id = likes.user_id
+GROUP BY likes.user_id
+HAVING COUNT(DISTINCT photo_id) = (SELECT COUNT(*) FROM photos);
