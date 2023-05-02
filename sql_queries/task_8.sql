0c74f211a9fba89d2d4caea3053b4004244fab05
Celia Cabello Navarro <118032871+celiacnavarro@users.noreply.github.com>
2023-05-02 08:31:31 +0200

    Update task_8.sql

--- sql_queries/task_8.sql
+++ sql_queries/task_8.sql
@@ -1,9 +1,6 @@
--- Write an SQL query to solve the given problem statement.
--- A brand wants to know which hashtags to use in a post. What are the top 5 most commonly used hashtags?
+-- Write an SQL query to solve the given problem statement
+-- Total numbers of users who have posted at least one time
 
-SELECT tags.tag_name, COUNT(*) AS count
-FROM photo_tags
-JOIN tags 
-ON photo_tags.tag_id = tags.id 
-GROUP BY photo_tags.tag_id
-ORDER BY count DESC
+SELECT COUNT(DISTINCT user_id) AS total_number_of_users_with_posts
+FROM photos
+
