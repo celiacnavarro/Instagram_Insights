21bbe4d6eb985ded428d92e7fd78c51d8448aa8e
Celia Cabello Navarro <118032871+celiacnavarro@users.noreply.github.com>
2023-05-02 08:30:11 +0200

    Update task_5.sql

--- sql_queries/task_5.sql
+++ sql_queries/task_5.sql
@@ -1,9 +1,4 @@
 -- Write an SQL query to solve the given problem statement.
--- user ranking by postings higher to lower
+-- Our Investors want to know...How many times does the average user post? (total number of photos/total number of users)
 
-SELECT users.username, COUNT(photos.image_url)
-FROM users
-LEFT JOIN photos ON users.id = photos.user_id
-GROUP BY users.id
-ORDER BY COUNT(photos.image_url) DESC
-LIMIT 74;
+SELECT ROUND((SELECT COUNT(*)FROM photos)/(SELECT COUNT(*) FROM users),2)
